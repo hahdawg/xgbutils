786a98f4df81146caf942c7b8d37bccb84363d3b
hahdawg <hahdawg@yahoo.com>
2017-06-15 12:04:34 -0500

    rename methods

--- xgbutils/model_selection.py
+++ xgbutils/model_selection.py
@@ -33,7 +33,7 @@ class ParamSearchCV(object):
         return "test-%s-mean" % self.__cv_metric
 
     @staticmethod
-    def _combine_params(hp_params, input_params):
+    def _update_params(hp_params, input_params):
         for param_name, param_val in hp_params.iteritems():
             input_params["params"][param_name] = param_val
         return input_params
@@ -44,7 +44,7 @@ class ParamSearchCV(object):
             if "max_depth" in kwargs:
                 kwargs["max_depth"] = int(kwargs["max_depth"])
 
-            params = self._combine_params(hp_params=kwargs, input_params=self.cv_params)
+            params = self._update_params(hp_params=kwargs, input_params=self.cv_params)
             bst = xgb.cv(dtrain=dtrain, **params)
             loss = bst[self._cv_metric].min()
             return loss
@@ -59,13 +59,13 @@ class ParamSearchCV(object):
         self.best_params_ = best_params_
 
         # choose number of trees
-        cv_params = self._combine_params(self.best_params_, self.cv_params)
+        cv_params = self._update_params(self.best_params_, self.cv_params)
         bst = xgb.cv(dtrain=dtrain, **cv_params)
         self.num_boost_round_ = bst[self._cv_metric].argmin()
         self.fit_params["num_boost_round"] = self.num_boost_round_
 
         # re-fit using number of trees found from cross validation
-        train_params = self._combine_params(hp_params=self.best_params_, input_params=self.fit_params)
+        train_params = self._update_params(hp_params=self.best_params_, input_params=self.fit_params)
         self.best_estimator_ = xgb.train(dtrain=dtrain, **train_params)
 
     def predict(self, dtest):
